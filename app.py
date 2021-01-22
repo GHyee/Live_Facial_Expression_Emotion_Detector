09bd8d7183304a39d61f0ffa08472ad724a6750a
GHyee <guohao.yee@gmail.com>
2021-01-22 17:29:54 +0800

    Add Webcam live feed to app.py

--- app.py
+++ app.py
@@ -64,5 +64,17 @@ def main():
 
     app_object_detection()
 
+    st.title("Webcam Live Feed")
+    
+    run = st.checkbox('Run')
+    FRAME_WINDOW = st.image([])
+    camera = cv2.VideoCapture(0)
+
+    while run:
+        _, frame = camera.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        FRAME_WINDOW.image(frame)
+    else:
+        st.write('Stopped')
 
 main()
